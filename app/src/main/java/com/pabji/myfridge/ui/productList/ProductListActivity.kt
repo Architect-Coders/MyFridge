816f486f4e7b962f1ad5d19b8169ea03c48d0503
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-03 01:14:29 +0100

    Update ProductListActivity
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/ui/productList/ProductListActivity.kt
+++ app/src/main/java/com/pabji/myfridge/ui/productList/ProductListActivity.kt
@@ -1,38 +1,35 @@
-package com.pabji.myfridge.ui.main
+package com.pabji.myfridge.ui.productList
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
-import com.google.android.material.snackbar.Snackbar
 import com.pabji.myfridge.R
+import com.pabji.myfridge.data.datasources.ProductDBDatasource
+import com.pabji.myfridge.ui.extensions.getViewModel
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class ProductListActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    private lateinit var viewModel: ProductListViewModel
+    private val adapter = ProductListAdapter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        viewModel = ViewModelProviders.of(this)[MainViewModel::class.java]
+        viewModel = getViewModel { ProductListViewModel(ProductDBDatasource(application)) }
         viewModel.viewState.observe(this, Observer(::updateUI))
+
+        rv_product_list.adapter = adapter
 
         fab.setOnClickListener { viewModel.onFabClicked() }
     }
 
     private fun updateUI(viewState: MainViewState?) {
         when (viewState) {
-            SnackBar -> showSnackBar()
+            is MyProductList -> adapter.productList = viewState.productList
         }
     }
-
-    private fun showSnackBar() {
-        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show()
-    }
-
-
 }
