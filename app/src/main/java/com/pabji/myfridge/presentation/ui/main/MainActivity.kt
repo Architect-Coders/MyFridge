5c2ed0776998d045ceeb5caafe2e858541f71275
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-07 17:39:50 +0100

    Improvements in Views
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
@@ -2,9 +2,9 @@ package com.pabji.myfridge.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.pabji.myfridge.R
 import com.pabji.myfridge.common.getViewModel
+import com.pabji.myfridge.common.setFragment
 import com.pabji.myfridge.data.datasources.ProductDBDatasource
 import com.pabji.myfridge.presentation.ui.productList.ProductListFragment
 import kotlinx.android.synthetic.main.activity_main.*
@@ -20,19 +20,8 @@ class MainActivity : AppCompatActivity() {
 
         viewModel = getViewModel { MainViewModel(ProductDBDatasource(application)) }
 
-        addFragment(ProductListFragment.newInstance(), true, ProductListFragment.TAG)
+        setFragment(ProductListFragment.newInstance(), R.id.fragment_container)
 
         fab.setOnClickListener { viewModel.onFabClicked() }
-    }
-
-    fun addFragment(fragment: Fragment, addToBackStack: Boolean, tag: String) {
-        val manager = supportFragmentManager
-        val ft = manager.beginTransaction()
-
-        if (addToBackStack) {
-            ft.addToBackStack(tag)
-        }
-        ft.replace(R.id.fragment_container, fragment, tag)
-        ft.commitAllowingStateLoss()
     }
 }
