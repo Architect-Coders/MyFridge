55080dc5dc05f6767645bd34e5541e8d0c7c44f9
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-07 18:34:30 +0100

    Create MainNavigator
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
@@ -4,9 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.pabji.myfridge.R
 import com.pabji.myfridge.common.getViewModel
-import com.pabji.myfridge.common.setFragment
-import com.pabji.myfridge.data.datasources.ProductDBDatasource
-import com.pabji.myfridge.presentation.ui.productList.ProductListFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,10 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        viewModel = getViewModel { MainViewModel(ProductDBDatasource(application)) }
-
-        setFragment(ProductListFragment.newInstance(), R.id.fragment_container)
-
+        viewModel = getViewModel { MainViewModel() }.also {
+            it.navigator = MainNavigator(this)
+        }
         fab.setOnClickListener { viewModel.onFabClicked() }
     }
 }
