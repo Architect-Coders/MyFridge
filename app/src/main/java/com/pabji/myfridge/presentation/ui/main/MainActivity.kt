4e781869a9bec3021bc9b97841829e4fafa7ffdb
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-09 17:22:22 +0100

    Integrate Insert Product to Database
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainActivity.kt
@@ -8,16 +8,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel: MainViewModel
+    lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        viewModel = getViewModel { MainViewModel() }.also {
-            it.navigator = MainNavigator(this)
-        }
-        fab.setOnClickListener { viewModel.onFabClicked() }
+        viewModel = getViewModel { MainViewModel() }
     }
 }
--- app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainViewModel.kt
+++ app/src/main/java/com/pabji/myfridge/presentation/ui/main/MainViewModel.kt
@@ -2,16 +2,4 @@ package com.pabji.myfridge.presentation.ui.main
 
 import com.pabji.myfridge.common.BaseViewModel
 
-class MainViewModel : BaseViewModel() {
-
-    var navigator: MainNavigator? = null
-
-    init {
-        navigator?.goToProductList()
-    }
-
-    fun onFabClicked() {
-        navigator?.goToCreateProduct(true)
-    }
-
-}
+class MainViewModel : BaseViewModel()
