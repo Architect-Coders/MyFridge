9d7c6d9c592f2d26f056867aae83aa9becfbcc90
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-10 22:26:13 +0100

    Rename presentation/ui to presentation/features
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/presentation/features/searchProducts/SearchProductsViewModel.kt
+++ app/src/main/java/com/pabji/myfridge/presentation/features/searchProducts/SearchProductsViewModel.kt
@@ -1,4 +1,4 @@
-package com.pabji.myfridge.presentation.ui.searchProducts
+package com.pabji.myfridge.presentation.features.searchProducts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
