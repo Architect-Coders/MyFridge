0b18ec2e07861a418a60de5bb8f591156ec11ecf
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-11 23:59:28 +0100

    change getRandomProducts by searchProductsByName with search term and pagination in ApiService
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/network/ApiService.kt
+++ app/src/main/java/com/pabji/myfridge/data/network/ApiService.kt
@@ -1,11 +1,14 @@
 package com.pabji.myfridge.data.network
 
 import com.pabji.myfridge.data.network.responses.SearchResponse
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface ApiService {
 
-    @GET("cgi/search.pl?search_simple=1&json=1&action=process")
-    suspend fun getRamdomProducts(): SearchResponse
+    @GET("cgi/search.pl?search_simple=1&json=1&action=process&fields=product_name,image_small_url")
+    suspend fun searchProductsByName(@Query("search_terms") name: String, @Query("page") page: Int): Response<SearchResponse>
+
 
 }
