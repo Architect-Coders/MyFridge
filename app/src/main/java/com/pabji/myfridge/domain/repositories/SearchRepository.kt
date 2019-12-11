268c1b0e64317b301c0954a7e49101518c36d64b
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-11 23:57:52 +0100

    change getRandomProducts by searchProductsByName with search term and pagination
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/domain/repositories/SearchRepository.kt
+++ app/src/main/java/com/pabji/myfridge/domain/repositories/SearchRepository.kt
@@ -1,7 +1,12 @@
 package com.pabji.myfridge.domain.repositories
 
+import arrow.core.Either
 import com.pabji.myfridge.domain.dtos.ProductDTO
+import com.pabji.myfridge.domain.errors.DomainError
 
 interface SearchRepository {
-    suspend fun getRandomProducts(): List<ProductDTO>
+    suspend fun searchProductsByName(
+        searchTerm: String,
+        page: Int = 1
+    ): Either<DomainError, List<ProductDTO>>
 }
