24a52968e8133f58e45fab7c0b4a5af383a04e3b
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-12 23:11:37 +0100

    Add previewUrl to ProductResponse and ProductEntity
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/network/responses/ProductResponse.kt
+++ app/src/main/java/com/pabji/myfridge/data/network/responses/ProductResponse.kt
@@ -2,4 +2,7 @@ package com.pabji.myfridge.data.network.responses
 
 import com.google.gson.annotations.SerializedName
 
-data class ProductResponse(@SerializedName("product_name") val productName: String = "")
+data class ProductResponse(
+    @SerializedName("product_name") val productName: String?,
+    @SerializedName("image_small_url") val previewUrl: String?
+)
