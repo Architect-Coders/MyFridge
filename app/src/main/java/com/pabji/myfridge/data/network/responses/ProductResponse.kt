fdd30fba958ec6c50d137c9a0d9de428b7b7d2c2
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-13 00:16:47 +0100

    Get product detail api call
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/network/responses/ProductResponse.kt
+++ app/src/main/java/com/pabji/myfridge/data/network/responses/ProductResponse.kt
@@ -4,5 +4,14 @@ import com.google.gson.annotations.SerializedName
 
 data class ProductResponse(
     @SerializedName("product_name") val productName: String?,
-    @SerializedName("image_small_url") val previewUrl: String?
+    @SerializedName("image_small_url") val previewUrl: String?,
+    @SerializedName("quantity") val quantity: String?,
+    @SerializedName("code") val barcode: String?,
+    @SerializedName("stores") val stores: String?,
+    @SerializedName("countries") val countries: String?,
+    @SerializedName("image_nutrition_url") val imageNutritionUrl: String?,
+    @SerializedName("generic_name") val genericName: String?,
+    @SerializedName("ingredients_text") val ingredientsText: String?,
+    @SerializedName("image_ingredients_url") val ingredientsUrl: String?,
+    @SerializedName("categories") val categories: String?
 )
