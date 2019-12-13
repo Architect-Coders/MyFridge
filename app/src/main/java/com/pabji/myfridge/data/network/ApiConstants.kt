a375a84b96fbb2eb07fb547f613e11c588fdffc9
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-13 20:19:46 +0100

    Fix some codestyles
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/network/ApiConstants.kt
+++ app/src/main/java/com/pabji/myfridge/data/network/ApiConstants.kt
@@ -1,5 +1,6 @@
 package com.pabji.myfridge.data.network
 
+const val PRODUCTS = "products"
 const val PRODUCT_NAME = "product_name"
 const val IMAGE_SMALL_URL = "image_small_url"
 const val QUANTITY = "quantity"
