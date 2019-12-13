472c27b3cfec6a52e339465b7813635f151e4db7
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-13 19:22:12 +0100

    Add getProductDetail to SearchRepository
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/datasources/SearchDatasource.kt
+++ app/src/main/java/com/pabji/myfridge/data/datasources/SearchDatasource.kt
@@ -4,21 +4,29 @@ import arrow.core.Either
 import arrow.core.extensions.either.applicativeError.applicativeError
 import arrow.core.extensions.either.bifunctor.bimap
 import arrow.integrations.retrofit.adapter.unwrapBody
+import com.pabji.myfridge.data.extensions.toProductDTO
 import com.pabji.myfridge.data.extensions.toProductDTOList
 import com.pabji.myfridge.data.network.ApiClient
 import com.pabji.myfridge.data.network.ApiService
+import com.pabji.myfridge.domain.errors.DetailError
 import com.pabji.myfridge.domain.errors.SearchError
 import com.pabji.myfridge.domain.repositories.SearchRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.Response
 
 class SearchDatasource : SearchRepository {
 
     private var client: ApiService = ApiClient.createService()
     override suspend fun searchProductsByName(searchTerm: String, page: Int) =
-        withContext(Dispatchers.IO) {
-            client.searchProductsByName(searchTerm, page)
-                .unwrapBody(Either.applicativeError())
-                .bimap({ SearchError }, { it.toProductDTOList() })
-        }
+            getHTTPResponse(client.searchProductsByName(searchTerm, page))
+                    .bimap({ SearchError }, { it.toProductDTOList() })
+
+    override suspend fun getProductDetail(productId: String) =
+            getHTTPResponse(client.getProductDetailById(productId))
+                    .bimap({ DetailError }, { it.toProductDTO() })
+
+
+    private suspend fun <T, R : Response<T>> getHTTPResponse(response: R) =
+            withContext(Dispatchers.IO) { response.unwrapBody(Either.applicativeError()) }
 }
--- app/src/main/java/com/pabji/myfridge/domain/errors/DomainError.kt
+++ app/src/main/java/com/pabji/myfridge/domain/errors/DomainError.kt
@@ -2,3 +2,5 @@ package com.pabji.myfridge.domain.errors
 
 sealed class DomainError
 object SearchError : DomainError()
+object DetailError : DomainError()
+
