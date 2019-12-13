d7123ad5c70ecb243bac3b4d6d11dc5650a51567
Pablo Jiménez Casado <pabjimcas@gmail.com>
2019-12-13 20:04:48 +0100

    Create ApiConstants
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- app/src/main/java/com/pabji/myfridge/data/database/entities/ProductEntity.kt
+++ app/src/main/java/com/pabji/myfridge/data/database/entities/ProductEntity.kt
@@ -1,12 +1,11 @@
 package com.pabji.myfridge.data.database.entities
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "products")
 data class ProductEntity(
-    @PrimaryKey(autoGenerate = true) val id: Long?,
-    @ColumnInfo(name = "name") val name: String?,
-    @ColumnInfo(name = "previewUrl") val previewUrl: String?
+        @PrimaryKey(autoGenerate = true) val id: Long?,
+        val name: String?,
+        val previewUrl: String?
 )
---
+++ app/src/main/java/com/pabji/myfridge/data/network/ApiConstants.kt
@@ -0,0 +1,13 @@
+package com.pabji.myfridge.data.network
+
+const val PRODUCT_NAME = "product_name"
+const val IMAGE_SMALL_URL = "image_small_url"
+const val QUANTITY = "quantity"
+const val CODE = "code"
+const val STORES = "stores"
+const val COUNTRIES = "countries"
+const val IMAGE_NUTRITION_URL = "image_nutrition_url"
+const val GENERIC_NAME = "generic_name"
+const val INGREDIENTS_TEXT = "ingredients_text"
+const val IMAGE_INGREDIENTS_URL = "image_ingredients_url"
+const val CATEGORIES = "categories"
