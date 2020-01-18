3d884769b574d6759f67e8d399f79c0642141fbc
Pablo Jiménez Casado <pabjimcas@gmail.com>
2020-01-18 11:29:52 +0100

    Add product to domain Module
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

---
+++ domain/src/main/java/com/pabji/domain/Product.kt
@@ -0,0 +1,18 @@
+package com.pabji.domain
+
+data class Product(
+    val barcode: String,
+    val name: String,
+    val previewUrl: String,
+    val imageUrl: String,
+    val quantity: String,
+    val stores: List<String>,
+    val countries: List<String>,
+    val imageNutritionUrl: String,
+    val brands: List<String>,
+    val genericName: String,
+    val ingredientsText: String,
+    val imageIngredientsUrl: String,
+    val categories: List<String>,
+    var existInFridge: Boolean
+)
