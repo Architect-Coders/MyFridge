c0b2e49309ca2044fa6200c3a040b51822d2af7f
Pablo Jiménez Casado <pabjimcas@gmail.com>
2020-01-26 18:24:51 +0100

    Create unit tests for modules for data module
    Signed-off-by: Pablo Jiménez Casado <pabjimcas@gmail.com>

--- data/src/main/java/com/pabji/data/datasources/RemoteDatasource.kt
+++ data/src/main/java/com/pabji/data/datasources/RemoteDatasource.kt
@@ -5,6 +5,6 @@ import com.pabji.domain.Either
 import com.pabji.domain.Product
 
 interface RemoteDatasource {
-    suspend fun searchProducts(searchTerm: String?, page: Int): Either<DomainError, List<Product>>
+    suspend fun searchProducts(searchTerm: String?): Either<DomainError, List<Product>>
     suspend fun getProductByBarcode(barcode: String): Either<DomainError, Product>
 }
--- data/src/main/java/com/pabji/data/repositories/ProductRepository.kt
+++ data/src/main/java/com/pabji/data/repositories/ProductRepository.kt
@@ -5,15 +5,10 @@ import com.pabji.domain.Either
 import com.pabji.domain.Product
 
 interface ProductRepository {
-    suspend fun getProducts(): Either<DomainError, List<Product>>
+    suspend fun getProducts(): List<Product>
     suspend fun saveProduct(product: Product)
     suspend fun removeProduct(product: Product)
-    suspend fun searchProducts(
-        searchTerm: String? = null,
-        page: Int = 1
-    ): Either<DomainError, List<Product>>
-
-    suspend fun getProductListByBarcodeList(barcodeList: List<String>): Either<DomainError, List<Product>>
+    suspend fun searchProducts(searchTerm: String? = null): List<Product>
     suspend fun getProductById(productId: Long): Either<DomainError, Product>
     suspend fun getProductByBarcode(barcode: String): Either<DomainError, Product>
 }
